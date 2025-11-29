1f741500ce76c290677764d14194af650759b2f3
Aakarsh Nainwal <nainwalaakarsh89@gmail.com>
2025-11-29 17:47:22 +0530

    Added Day 97 Code

---
+++ Day_97/code147.c
@@ -0,0 +1,39 @@
+/*
+Q147: Store employee data in a binary file using fwrite() and read using fread().
+
+Sample Test Cases:
+Input 1:
+Employee details entered and stored in file.
+Output 1:
+Displays employee data read from file.
+*/
+
+#include <stdio.h>
+
+struct Employee
+{
+    char name[20];
+    int id;
+    float salary;
+};
+
+int main()
+{
+    struct Employee e, temp;
+    FILE *fp;
+
+    scanf("%s %d %f", e.name, &e.id, &e.salary);
+
+    fp = fopen("employee.dat", "wb");
+    fwrite(&e, sizeof(struct Employee), 1, fp);
+    fclose(fp);
+
+    fp = fopen("employee.dat", "rb");
+    fread(&temp, sizeof(struct Employee), 1, fp);
+    fclose(fp);
+
+    printf("Name: %s | ID: %d | Salary: %.2f\n",
+           temp.name, temp.id, temp.salary);
+
+    return 0;
+}
